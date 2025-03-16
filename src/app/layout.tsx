788f71f35d7dd055cebb0fe8a3cb992742c9ab04
tarchevskyivan <i.tarchevsky@gmail.com>
2025-03-16 19:32:47 +0300

    Для адаптации добавил в head viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body>
         <Header />
         {children}
