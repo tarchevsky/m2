dc8c5b6ff673807b4b64da1d3fe9ddf80bdcb480
tarchevskyivan <i.tarchevsky@gmail.com>
2025-03-16 15:23:14 +0300

    Убрал старые шрифты и добавил новые

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,37 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import Header from '@/components/header/Header'
 import Footer from '@/components/footer/Footer'
+import Header from '@/components/header/Header'
+import Metrika from '@/components/metrika/Metrika'
 import ScrollToTop from '@/components/scrollToTop/ScrollToTop'
 import { SITE_NAME } from '@/constants/site.constants'
+import type { Metadata } from 'next'
 import { ReactNode } from 'react'
-import Metrika from '@/components/metrika/Metrika'
+import './globals.css'
+// Supports weights 100-900
+import '@fontsource-variable/raleway'
 
 const yId = process.env.NEXT_PUBLIC_YID // id яндекс метрики
 
 export const metadata: Metadata = {
-	title: {
-		default: SITE_NAME,
-		template: `%s | ${SITE_NAME}`
-	}
+  title: {
+    default: SITE_NAME,
+    template: `%s | ${SITE_NAME}`,
+  },
 }
 
 export default function RootLayout({
-	children
+  children,
 }: Readonly<{
-	children: ReactNode
+  children: ReactNode
 }>) {
-	return (
-		<html lang='ru'>
-			<body>
-				<Header />
-				{children}
-				<ScrollToTop />
-				<Footer />
-				{yId ? <Metrika yId={yId} /> : null}
-			</body>
-		</html>
-	)
+  return (
+    <html lang="ru">
+      <body>
+        <Header />
+        {children}
+        <ScrollToTop />
+        <Footer />
+        {yId ? <Metrika yId={yId} /> : null}
+      </body>
+    </html>
+  )
 }
